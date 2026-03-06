0b09e4925235471e45c884f0ae68be301e839070
MoJOS <ausiankou@gmail.com>
2026-03-06 22:04:58 +0300

    [User Service] DAO Layer

---
+++ src/main/java/com/ausiankou/controllers/UserController.java
@@ -0,0 +1,79 @@
+package com.ausiankou.controllers;
+
+import com.ausiankou.entity.PaymentCard;
+import com.ausiankou.entity.User;
+import com.ausiankou.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{userId}/cards")
+    public ResponseEntity<PaymentCard> addCardToUser(
+            @PathVariable Long userId,
+            @RequestBody PaymentCard card) {
+        return new ResponseEntity<>(userService.createCard(userId, card),
+                HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<User>> getAllUsers(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String surname,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(userService.getAllUsers(name, surname, pageable));
+    }
+
+    @GetMapping("/{userId}/cards")
+    public ResponseEntity<List<PaymentCard>> getUserCards(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.getCardsByUserId(userId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable Long id,
+            @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    @PatchMapping("/{id}/activate")
+    public ResponseEntity<Void> activateUser(@PathVariable Long id) {
+        userService.activateUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<Void> deactivateUser(@PathVariable Long id) {
+        userService.deactivateUser(id);
+        return ResponseEntity.ok().build();
+    }
+}
