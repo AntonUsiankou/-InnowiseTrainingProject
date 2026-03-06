8068a5542edef2898e6607d10183ca1fd192cc0a
MoJOS <ausiankou@gmail.com>
2026-03-06 11:23:38 +0300

    feat: проджект

---
+++ src/main/java/com/ausiankou/InnowiseTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.ausiankou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseTrainingApplication.class, args);
+    }
+
+}
