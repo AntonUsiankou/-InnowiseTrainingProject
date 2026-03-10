1d8f800fb84e747cc5fab4718c6eb1919e4a9f77
MoJOS <ausiankou@gmail.com>
2026-03-10 13:16:34 +0300

    feat: Microservice architecture + Authentication Service
    fix: user-service fixes after review

---
+++ auth-service/src/main/java/com/ausiankou/exception/CustomExceptions.java
@@ -0,0 +1,23 @@
+package com.ausiankou.exception;
+
+public class CustomExceptions {
+
+    public static class UnauthorizedActionException extends RuntimeException {
+        public UnauthorizedActionException(String message) {
+            super(message);
+        }
+    }
+
+    public static class ResourceNotFoundException extends RuntimeException {
+        private final String resourceName;
+        private final String fieldName;
+        private final Object fieldValue;
+
+        public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+            super(String.format("%s не найден с %s: '%s'", resourceName, fieldName, fieldValue));
+            this.resourceName = resourceName;
+            this.fieldName = fieldName;
+            this.fieldValue = fieldValue;
+        }
+    }
+}
