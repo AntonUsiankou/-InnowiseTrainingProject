376dab97314877d1d8fbed4fcff70e361edb81f6
MoJOS <ausiankou@gmail.com>
2026-03-12 23:53:45 +0300

    feat: [User Service] Containerization

--- src/main/java/com/ausiankou/config/RedisConfig.java
+++ src/main/java/com/ausiankou/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.ausiankou.config;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +9,6 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
-import tools.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
 
